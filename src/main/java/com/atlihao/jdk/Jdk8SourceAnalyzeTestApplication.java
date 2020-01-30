42ccc2fa4e8945de1105828472283929a6102d8b
lihao7264 <1178454016@qq.com>
2020-01-30 16:17:29 +0800

    first commit

---
+++ src/main/java/com/atlihao/jdk/Jdk8SourceAnalyzeTestApplication.java
@@ -0,0 +1,13 @@
+package com.atlihao.jdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk8SourceAnalyzeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk8SourceAnalyzeTestApplication.class, args);
+    }
+
+}
